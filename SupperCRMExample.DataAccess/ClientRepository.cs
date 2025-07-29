a541a3587482dfeba78f3356f7a8ef8afb386536
metndurn <mtndrn123@gmail.com>
2025-07-29 21:22:28 +0300

    Services Katmanında ClientService Yapısı

--- SupperCRMExample.DataAccess/ClientRepository.cs
+++ SupperCRMExample.DataAccess/ClientRepository.cs
@@ -7,9 +7,14 @@ using SupperCRMExample.Entities.Abstract;
 
 namespace SupperCRMExample.DataAccess
 {
+	/*dipnot bız burada bunları kaldırırsak proje yine de sorunsuz sekılde calısacaktır ozel olarakta
+	 buraya ihtiyaca gore kodlar yazabıleyım*/
+	public interface ICleintRepository : IRepository<Client> //ClientRepository sınıfı, IRepository<Client> arayüzünü uygular, yani Client nesneleri ile çalışır.
+	{
+		//Burada ClientRepository sınıfına özel metotlar tanımlanabilir buraya yazılan kdolar ClientRepository sınıfında kullanılabilir.
+	}
 
-
-	public class ClientRepository : Repository<Client>, IRepository<Client>/*yukarıdaki IRepository interface ile client ile ilgili olan herseyi  burada goruyoruz*/
+	public class ClientRepository : Repository<Client>, ICleintRepository/*yukarıdaki IRepository interface ile client ile ilgili olan herseyi  burada goruyoruz*/
 	{
 		private readonly DatabaseContext _context;// DatabaseContext sınıfından bir nesne tanımlıyoruz, bu sınıf veritabanı bağlantısını ve işlemlerini yönetir.
 		public ClientRepository(DatabaseContext context) : base(context)/*nesne turetılerek context yapısı olusturuldu base sonradan eklendi */
--- SupperCRMExample.Services/ClientService.cs
+++ SupperCRMExample.Services/ClientService.cs
@@ -1,18 +1,40 @@
-﻿using SupperCRMExample.Entities;
+﻿using SupperCRMExample.DataAccess;
+using SupperCRMExample.DataAccess.Abstract;
+using SupperCRMExample.Entities;
 
 namespace SupperCRMExample.Services
 {
-	public class ClientService//client hakkında hersey burada yazılacak
+	public interface IClientService
+	{
+		void Create(string name, string email);
+		List<Client> List();
+	}
+
+	public class ClientService : IClientService
+	//client hakkında hersey burada yazılacak
 	{
 		//CRUD işlemleri için gerekli metotlar burada tanımlanacak
 
-		public ClientService()
+		private readonly ICleintRepository _repository;// ICleintRepository arayüzünden bir repository nesnesi tanımlıyoruz, bu sayede ClientRepository sınıfının metotlarını kullanabileceğiz.
+
+		public ClientService(ICleintRepository repository)//kullanıcıya ait olan repository sınıfını alır ve bu sınıfı kullanarak CRUD işlemlerini yapar
 		{
-			
+			_repository = repository;
 		}
 		public List<Client> List()//listeleme metodur
 		{
+			return _repository.GetAll();//repository üzerinden tüm clientları getirir
+		}
 
+		public void Create(string name, string email)
+		{
+			Client client = new Client
+			{
+				Name = name,
+				Email = email,
+				CreatedAt = DateTime.Now,
+			};
+			_repository.Add(client);//repository üzerinden yeni bir client ekler
 		}
 	}
 }
